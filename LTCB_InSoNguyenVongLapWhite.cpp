a2fc5f4dafd0ae8b9cd68ac2dd63b1b8ed19d9b4
TamNhien <canhthien91@gmail.com>
2024-03-05 19:36:34 +0700

    Add project files.

---
+++ LTCB_InSoNguyenVongLapWhite.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 2;
+    cout << "Nhap so nguyen duong n: "; cin >> n;
+    cout << "Cac so nguyen to tu 0 den " << n << " la:\n";
+
+    while (i <= n) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; ++j)
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        if (isPrime) cout << i << " ";
+        ++i;
+    }
+
+    return 0;
+}
